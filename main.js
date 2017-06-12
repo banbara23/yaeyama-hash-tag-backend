79f6f0e84db7afb1f7df5f8d6e94d7392fa07bae
池村和剛 <z1tg6774@h1408327.local>
2017-06-12 11:22:00 +0900

    スクレイピングしてfirebase送信まで実装

--- main.js
+++ main.js
@@ -1,7 +1,48 @@
-var ig = require('instagram-tagscrape');
+const ig = require('instagram-tagscrape');
+const firebase = require('firebase');
+const config = require("./firebase-config.js")
 
-const tag = "%E5%85%AB%E9%87%8D%E5%B1%B1"; // 八重山
+/**
+ * 初期処理
+ */
+function initialize() {
+    firebase.initializeApp(config);
+}
 
-ig.scrapeTagPage(tag).then(function (result) {
-    console.dir(result);
-})
+const tag_yaeyama = "%E5%85%AB%E9%87%8D%E5%B1%B1"; // 八重山
+
+/**
+ * スクレイピングして結果をpromiseで返す
+ * @param {string} tag タグ文字列
+ */
+function scraping(tag) {
+    return ig.scrapeTagPage(tag)
+        .then((result) => {
+            console.log("scraping ok")
+            return result;
+        })
+}
+
+/**
+ * 引数をFirebase Databaseに送信する
+ * @param {object} data 
+ */
+function sendToFirebase(data) {
+    return firebase.database()
+        .ref('/')
+        .set(data, function () {
+            firebase.database().goOffline()
+            console.log('send ok')
+        })
+}
+
+/**
+ * メイン処理
+ */
+Promise.resolve()
+    .then(console.log('main start'))
+    .then(initialize())
+    .then(() => scraping(tag_yaeyama))
+    .then(data => sendToFirebase(data))
+    .then(() => console.log('main end'))
+    .catch(err => console.error(err))
