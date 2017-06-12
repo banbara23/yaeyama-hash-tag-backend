fd3c3ac056dcbc8d75bc484b947217f22b433fb4
池村和剛 <z1tg6774@h1408327.local>
2017-06-12 12:09:30 +0900

    hashtags.jsにタグ情報を移動

---
+++ hashtags.js
@@ -0,0 +1,48 @@
+// tagにはnameの文字をURLエンコードした値をセットする
+module.exports = [
+  {
+    id: 'yaeyama',
+    name: "八重山",
+    tag: "%E5%85%AB%E9%87%8D%E5%B1%B1"
+  },
+  {
+    id: 'taketomicho',
+    name: "竹富町",
+    tag: "%e7%ab%b9%e5%af%8c%e7%94%ba"
+  },
+  {
+    id: 'ishigaki',
+    name: "石垣島",
+    tag: "%e7%9f%b3%e5%9e%a3%e5%b3%b6"
+  },
+  {
+    id: 'iriomote',
+    name: "西表島",
+    tag: "%e8%a5%bf%e8%a1%a8%e5%b3%b6"
+  },
+  {
+    id: 'taketomi',
+    name: "竹富島",
+    tag: "%e7%ab%b9%e5%af%8c%e5%b3%b6"
+  },
+  {
+    id: 'kohama',
+    name: "小浜島",
+    tag: "%e5%b0%8f%e6%b5%9c%e5%b3%b6"
+  },
+  {
+    id: 'kuroshima',
+    name: "黒島",
+    tag: "%e9%bb%92%e5%b3%b6"
+  },
+  {
+    id: 'hatoma',
+    name: "鳩間島",
+    tag: "%e9%b3%a9%e9%96%93%e5%b3%b6"
+  },
+  {
+    id: 'hateruma',
+    name: "波照間島",
+    tag: "%e6%b3%a2%e7%85%a7%e9%96%93%e5%b3%b6"
+  }
+]
