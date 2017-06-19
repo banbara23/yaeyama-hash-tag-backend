28f9db179e3c7240af105e03eee14e1c81db10eb
ikemura <banbara.work@gmail.com>
2017-06-19 15:12:07 +0900

    instagram-searchtagsでスクレイピングして送信まで実装

--- hashtags.js
+++ hashtags.js
@@ -1,48 +1,53 @@
 // tagにはnameの文字をURLエンコードした値をセットする
-module.exports = [
-  {
+module.exports = {
+  yaeyama: {
     id: 'yaeyama',
     name: "八重山",
     tag: "%E5%85%AB%E9%87%8D%E5%B1%B1"
   },
-  {
+  taketomicho: {
     id: 'taketomicho',
     name: "竹富町",
     tag: "%e7%ab%b9%e5%af%8c%e7%94%ba"
   },
-  {
+  ishigaki: {
     id: 'ishigaki',
     name: "石垣島",
     tag: "%e7%9f%b3%e5%9e%a3%e5%b3%b6"
   },
-  {
+  iriomote: {
     id: 'iriomote',
     name: "西表島",
     tag: "%e8%a5%bf%e8%a1%a8%e5%b3%b6"
   },
-  {
+  taketomi: {
     id: 'taketomi',
     name: "竹富島",
     tag: "%e7%ab%b9%e5%af%8c%e5%b3%b6"
   },
-  {
+  kohama: {
     id: 'kohama',
     name: "小浜島",
     tag: "%e5%b0%8f%e6%b5%9c%e5%b3%b6"
   },
-  {
+  kuroshima: {
     id: 'kuroshima',
     name: "黒島",
     tag: "%e9%bb%92%e5%b3%b6"
   },
-  {
+  hatoma: {
     id: 'hatoma',
     name: "鳩間島",
     tag: "%e9%b3%a9%e9%96%93%e5%b3%b6"
   },
-  {
+  hateruma: {
     id: 'hateruma',
     name: "波照間島",
     tag: "%e6%b3%a2%e7%85%a7%e9%96%93%e5%b3%b6"
+  },
+  yonaguni: {
+    id: 'yonaguni',
+    name: "与那国島",
+    tag: "%97%5E%93%DF%8D%91%93%87"
   }
-]
+}
