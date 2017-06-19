3d1c83c6fdcc707b4e10f5133c1389f46287fc1e
ikemura <banbara.work@gmail.com>
2017-06-19 13:31:54 +0900

    指定件数のスクレイピング成功

--- test.js
+++ test.js
@@ -1,6 +1,37 @@
-const moment = require("moment")
-
-const timestamp = "1497428291"
-
-console.log(moment.locale('ja'))
-console.log(moment.unix(timestamp).format('LLL'))
+// DEBUG=instagramsearchtags node test.js
+const InstagramSearchTags = require('instagram-searchtags')
+ 
+// Create instance with your credentials 
+const searchTags = new InstagramSearchTags({
+  username: 'banbara23',
+  password: 'banbara',
+})
+ 
+// Login Instagram with credentials 
+searchTags.login()
+  .then(() => {
+ 
+    // Create #dog tag 
+    const tag = searchTags.createTag('dog')
+ 
+    // Fetch 10 latest nodes 
+    return tag.fetchNodes(30)
+ 
+  })
+  .then((nodes) => {
+ 
+    // ... do something cool with nodes 
+    console.log(nodes.length)
+ 
+    // close connection 
+    searchTags.close()
+ 
+  })
+  .catch((err) => {
+ 
+    // close connection 
+    searchTags.close()
+ 
+    console.error(`Error: ${err.message}`)
+ 
+  })
