d4d5a45415bfda252ea7085fe872bbd8be87531b
ikemura <banbara.work@gmail.com>
2017-07-10 07:30:15 +0900

    add config

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: "AIzaSyDXqSIzwGY8KkVtKRWUCw0Yqk3oapvJqPc",
+  authDomain: "yaeyama-hash-tag.firebaseapp.com",
+  databaseURL: "https://yaeyama-hash-tag.firebaseio.com",
+  projectId: "yaeyama-hash-tag",
+  storageBucket: "yaeyama-hash-tag.appspot.com",
+  messagingSenderId: "371952419741"
+};
